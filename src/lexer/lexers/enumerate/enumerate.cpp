d008458c6634b8703f2f587a06e7910523b09359
shrimpster00 <caydenlund@gmail.com>
2021-10-07 19:13:55 -0600

    Add the Token class to Enumerate.

--- src/lexer/lexers/enumerate/enumerate.cpp
+++ src/lexer/lexers/enumerate/enumerate.cpp
@@ -1,8 +1,8 @@
 // src/lexer/lexers/enumerate/enumerate.cpp
-// v. 0.2.0
+// v. 0.3.0
 //
 // Author: Cayden Lund
-//   Date: 10/06/2021
+//   Date: 10/07/2021
 //
 // This file is part of mark-sideways, a new markup/markdown language
 // for quickly writing and formatting notes.
@@ -15,7 +15,6 @@
 // System imports.
 #include <string>
 #include <regex>
-#include <iostream>
 
 // The enumerate header file.
 #include "enumerate.hpp"
@@ -39,8 +38,8 @@ namespace mark_sideways
         mark_sideways::lexers::Enumerate::Enumerate(mark_sideways::State *state) : AbstractLexer(state)
         {
             // The regex for the bullet point lexer.
-            this->match_regex = std::regex("^(\\s*)\\d+\\.(\\s.*)");
-            this->escape_regex = std::regex("^(\\s*)\\\\(\\d+\\.\\s.*)");
+            this->match_regex = std::regex("^(\\s*)\\d+\\.\\s+([^]*)$");
+            this->escape_regex = std::regex("^\\s*\\\\(\\d+\\.\\s+)([^]*)$");
         }
 
         // The class destructor.
@@ -55,58 +54,57 @@ namespace mark_sideways
         // * return bool      - True if the line is a bullet point.
         bool mark_sideways::lexers::Enumerate::is_enumerate(std::string line)
         {
-            if (this->ignore(line))
-                return false;
-            return std::regex_search(line, this->match_regex);
+            // To be a bullet point, the state must *not* be in the verbatim state and *not* be in the math state,
+            // and the line must match the regular expression.
+            return (!this->state->is_verbatim() && !this->state->is_math() && std::regex_match(line, this->match_regex));
         }
 
         // lex() is used to lex a single input line.
-        // This method takes in a string and returns a new string with the lexed tokens.
+        // This method takes in a string and returns a new vector with the lexed tokens.
         //
-        // * std::string line   - The string to lex.
-        // * return std::string - The lexed string.
-        std::string mark_sideways::lexers::Enumerate::lex(std::string line)
+        // * std::string line                         - The string to lex.
+        // * return std::vector<mark_sideways::Token> - The vector of tokens.
+        std::vector<mark_sideways::Token> mark_sideways::lexers::Enumerate::lex(std::string line)
         {
+            // The vector of tokens to return.
+            std::vector<mark_sideways::Token> tokens;
+
             // First, determine whether we are in a block of verbatim text or math mode.
-            if (!this->ignore(line))
+            if (this->state->is_verbatim() || this->state->is_math())
+            {
+                tokens.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, line));
+            }
+            else
             {
                 // If we are not in verbatim or math mode, then we can test whether the line is a bullet point.
-                if (std::regex_search(line, this->match_regex))
+                if (std::regex_match(line, this->match_regex))
                 {
                     // We can lex the line if it matches the regex.
                     std::string bullet_indentation = std::regex_replace(line, this->match_regex, "$1");
                     int bullet_point_level = (bullet_indentation.length() / 2) + 1;
 
-                    std::string bullet_content = std::regex_replace(line, this->match_regex, "$2");
+                    std::string rest = std::regex_replace(line, this->match_regex, "$2");
 
-                    return get_token(bullet_point_level) + bullet_content;
+                    // Add the bullet point token.
+                    tokens.push_back(mark_sideways::Token(mark_sideways::Token::token_type::ENUMERATE, std::to_string(bullet_point_level)));
+
+                    // Add the rest as an unlexed token.
+                    tokens.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, rest));
                 }
-                else if (std::regex_search(line, this->escape_regex))
+                else if (std::regex_match(line, this->escape_regex))
                 {
-                    // Otherwise, if the line matches the escape regex, then we delete the escape character.
-                    return std::regex_replace(line, this->escape_regex, "$1$2");
+                    // Otherwise, if the line matches the escape regex, then we add the bullet literal as a text token.
+                    std::string bullet_literal = std::regex_replace(line, this->escape_regex, "$1");
+                    std::string rest = std::regex_replace(line, this->escape_regex, "$2");
+
+                    // Add the text content token.
+                    tokens.push_back(mark_sideways::Token(mark_sideways::Token::token_type::TEXT_CONTENT, bullet_literal));
+
+                    // Add the rest as an unlexed token.
+                    tokens.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, rest));
                 }
             }
-            // Otherwise, return the line unchanged.
-            return line;
-        }
-
-        // ignore() returns true when we should ignore the given line.
-        //
-        // * std::string line - The line to test.
-        // * return bool      - Whether we should ignore the given line.
-        bool mark_sideways::lexers::Enumerate::ignore(std::string line)
-        {
-            return (state->is_verbatim() || state->is_math());
-        }
-
-        // The token used to replace the bullet point.
-        // The token is of the form "[%%BULLET_{LEVEL}%%]", where {LEVEL} is the bullet point level.
-        //
-        // * int level - The level of the bullet point.
-        std::string mark_sideways::lexers::Enumerate::get_token(int level)
-        {
-            return "[%%ENUMERATE=" + std::to_string(level) + "%%]";
+            return tokens;
         }
     }
 }
--- src/lexer/lexers/enumerate/enumerate.test.cpp
+++ src/lexer/lexers/enumerate/enumerate.test.cpp
@@ -1,8 +1,8 @@
 // src/lexer/lexers/enumerate/enumerate.test.cpp
-// v. 0.2.0
+// v. 0.3.0
 //
 // Author: Cayden Lund
-//   Date: 10/06/2021
+//   Date: 10/07/2021
 //
 // This file is part of mark-sideways, a new markup/markdown language
 // for quickly writing and formatting notes.
@@ -12,14 +12,43 @@
 // Copyright (C) 2021 Cayden Lund <https://github.com/shrimpster00>
 // License: MIT (https://opensource.org/licenses/MIT)
 
+// System imports.
+#include <string>
+#include <regex>
+#include <vector>
+
 // The GTest testing framework.
 #include <gtest/gtest.h>
 
 // We use the State class to keep track of the current state of the parser.
 #include "parser/state.hpp"
 
+// We use the Token class to build a vector of tokens.
+#include "lexer/token/token.hpp"
+
 // The Enumerate lexer class.
 #include "enumerate.hpp"
+
+// We define a static helper method to determine whether two vectors of tokens are equal.
+//
+// * std::vector<mark_sideways::Token> tokens1 - The first vector of tokens.
+// * std::vector<mark_sideways::Token> tokens2 - The second vector of tokens.
+// * return bool                               - True when the two vectors are equal; false otherwise.
+static bool tokens_equal(std::vector<mark_sideways::Token> tokens1, std::vector<mark_sideways::Token> tokens2)
+{
+    if (tokens1.size() != tokens2.size())
+    {
+        return false;
+    }
+    for (size_t i = 0; i < tokens1.size(); i++)
+    {
+        if (tokens1[i] != tokens2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Test the constructor.
 // Ensures that the lexer does not throw an exception.
@@ -38,14 +67,18 @@ TEST(EnumerateLexer, LexSingleEnumerateNoIndent)
     mark_sideways::lexers::Enumerate enumerate(&state);
 
     std::string input = "1. List item.";
-    std::string expected = "[%%ENUMERATE=1%%] List item.";
-    std::string actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    std::vector<mark_sideways::Token> expected;
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::ENUMERATE, "1"));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item."));
+    std::vector<mark_sideways::Token> actual = enumerate.lex(input);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 
     input = "2. List item.\n";
-    expected = "[%%ENUMERATE=1%%] List item.\n";
+    expected.clear();
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::ENUMERATE, "1"));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item.\n"));
     actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 }
 
 // Test the lex() function.
@@ -56,14 +89,18 @@ TEST(EnumerateLexer, LexSingleEnumerateIndent)
     mark_sideways::lexers::Enumerate enumerate(&state);
 
     std::string input = "  1. List item.";
-    std::string expected = "[%%ENUMERATE=2%%] List item.";
-    std::string actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    std::vector<mark_sideways::Token> expected;
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::ENUMERATE, "2"));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item."));
+    std::vector<mark_sideways::Token> actual = enumerate.lex(input);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 
     input = "    1. List item.\n";
-    expected = "[%%ENUMERATE=3%%] List item.\n";
+    expected.clear();
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::ENUMERATE, "3"));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item.\n"));
     actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 }
 
 // Test the lex() function.
@@ -74,14 +111,18 @@ TEST(EnumerateLexer, LexEscapedEnumerate)
     mark_sideways::lexers::Enumerate enumerate(&state);
 
     std::string input = "\\1. List item.";
-    std::string expected = "1. List item.";
-    std::string actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    std::vector<mark_sideways::Token> expected;
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::TEXT_CONTENT, "1. "));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item."));
+    std::vector<mark_sideways::Token> actual = enumerate.lex(input);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 
     input = "    \\1. List item.\n";
-    expected = "    1. List item.\n";
+    expected.clear();
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::TEXT_CONTENT, "1. "));
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "List item.\n"));
     actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 }
 
 // Test the lex() function.
@@ -94,14 +135,16 @@ TEST(EnumerateLexer, LexEnumerateInMathMode)
     state.toggle_math();
 
     std::string input = "1. List item.";
-    std::string expected = "1. List item.";
-    std::string actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    std::vector<mark_sideways::Token> expected;
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "1. List item."));
+    std::vector<mark_sideways::Token> actual = enumerate.lex(input);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 
     input = "    1. List item.\n";
-    expected = "    1. List item.\n";
+    expected.clear();
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "    1. List item.\n"));
     actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 }
 
 // Test the lex() function.
@@ -114,12 +157,14 @@ TEST(EnumerateLexer, LexEnumerateInVerbatimMode)
     state.toggle_verbatim();
 
     std::string input = "1. List item.";
-    std::string expected = "1. List item.";
-    std::string actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    std::vector<mark_sideways::Token> expected;
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "1. List item."));
+    std::vector<mark_sideways::Token> actual = enumerate.lex(input);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 
     input = "    1. List item.\n";
-    expected = "    1. List item.\n";
+    expected.clear();
+    expected.push_back(mark_sideways::Token(mark_sideways::Token::token_type::UNLEXED, "    1. List item.\n"));
     actual = enumerate.lex(input);
-    EXPECT_EQ(expected, actual);
+    EXPECT_TRUE(tokens_equal(expected, actual));
 }
