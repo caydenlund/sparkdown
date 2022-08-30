2d6e0b52a35ef25ac0e18d8e62ceb5cf1e63c081
Cayden Lund <cayden.lund@utah.edu>
2022-08-30 17:20:00 -0600

    Rewrite the `token` class.
    Unit tests not yet implemented.

--- src/token/token.cpp
+++ src/token/token.cpp
@@ -1,208 +1,88 @@
-// //token
-//
-// Author: Cayden Lund
-//
-// This file is part of sparkdown, a new markup/markdown language
-// for quickly writing and formatting notes.
-//
-// This file contains the implementation of the Token class.
-//
-// Copyright (C) 2021 Cayden Lund <https://github.com/shrimpster00>
-// License: MIT <https://opensource.org/licenses/MIT>
+/**
+ * @file token/token.cpp
+ * @package //token:token
+ * @author Cayden Lund <cayden.lund@utah.edu>
+ * @brief `token` class definition.
+ * @details This project is part of Sparkdown, a new markup language
+ *     for quickly writing and formatting notes.
+ *
+ *     This file implements the `token` class, which represents a single token
+ *     in the lexing/parsing process.
+ *
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @copyright 2021-2022 by Cayden Lund <https://github.com/caydenlund>
+ */
 
-// System imports.
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iostream>
+// TODO: Remove temporary pragmas once we no longer need them.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "readability-convert-member-functions-to-static"
 
-// The header for the Token class.
 #include "token.hpp"
 
-namespace sparkdown
-{
-    // constructor
-    //    The one-argument constructor saves the type of the token.
-    //
-    // Parameters:
-    //    const token_type &type - The type of the token.
-    Token::Token(const token_type &type) : type(type), value() {}
+namespace sparkdown {
 
-    // constructor
-    //    The two-argument constructor saves the type and value of the token.
-    //
-    // Parameters:
-    //    const token_type &type        - The type of the token.
-    //    const std::string_view &value - The value of the token.
-    //       OR
-    //    const char &value             - The value of the token.
-    Token::Token(const token_type &type, const std::string &value) : type(type), value()
-    {
-        this->value << value;
-    }
-    Token::Token(const token_type &type, const char &value) : type(type), value()
-    {
-        this->value << value;
-    }
+// TODO: Correctly initialize `type`.
+token::token(char character) : type(get_type(character)), value(character) {}
 
-    // constructor
-    //    The copy constructor saves the type and value of the token.
-    //
-    // Parameters:
-    //    const Token &other - The token to copy.
-    Token::Token(const Token &other) : type(other.type), value()
-    {
-        this->value << other.value.str();
-    }
+token::token(const token &other) = default;
 
-    // merge
-    //    Merges two tokens together.
-    //
-    // Parameters:
-    //    const Token &other - The token to merge with.
-    void Token::merge(const Token &other)
-    {
-        this->value << other.value.str();
-    }
+bool token::operator==(const token &other) const {
+    return type == other.type && value == other.value;
+}
 
-    // get_type
-    //    Returns the type of the token.
-    //
-    // Returns:
-    //    token_type - The type of the token.
-    token_type Token::get_type() const
-    {
-        return this->type;
-    }
+bool token::operator!=(const token &other) const { return !(*this == other); }
 
-    // get_value
-    //    Returns the string value of the token.
-    //
-    // Returns:
-    //    std::string - The value of the token.
-    std::string Token::get_value() const
-    {
-        return this->value.str();
-    }
-
-    // = operator
-    //    Allow for assignment of tokens.
-    //
-    // Parameters:
-    //    const Token &other - The token to assign.
-    void Token::operator=(const Token &other)
-    {
-        this->type = other.type;
-        this->value << other.value.str();
-    }
-
-    // == operator
-    //    Compare two tokens for equality.
-    //
-    // Parameters:
-    //    const Token &other - The token with which to compare.
-    //
-    // Returns:
-    //    bool - True when the tokens are equal.
-    bool Token::operator==(const Token &other) const
-    {
-        return this->type == other.type && this->value.str() == other.value.str();
-    }
-
-    // != operator
-    //    Compare two tokens for inequality.
-    //
-    // Parameters:
-    //    const Token &other - The token with which to compare.
-    //
-    // Returns:
-    //    bool - True when the tokens are not equal.
-    bool Token::operator!=(const Token &other) const
-    {
-        return !(*this == other);
-    }
-
-    // get_token_type
-    //    Returns the string representation of the token type.
-    //
-    // Parameters:
-    //    const token_type &type - The type of the token.
-    //
-    // Returns:
-    //    std::string - The string representation of the token type.
-    std::string Token::get_token_type(const token_type &type)
-    {
-        switch (type)
-        {
-        case token_type::SPACE:
-            return "Space";
-        case token_type::DOLLAR:
-            return "Dollar";
-        case token_type::COLON:
-            return "Colon";
-        case token_type::HASH:
-            return "Hash";
-        case token_type::STAR:
-            return "Asterisk";
-        case token_type::DASH:
-            return "Dash";
-        case token_type::EQUALS:
-            return "Equals";
-        case token_type::ESCAPE:
-            return "Backslash";
-        case token_type::NUMBER:
-            return "Number";
-        case token_type::TERMINAL:
-            return "Literal";
+token_type token::get_type(char character) {
+    switch (character) {
+        case ' ':
+        case '\t':
+            return token_type::SPACE;
+        case '$':
+            return token_type::DOLLAR;
+        case ':':
+            return token_type::COLON;
+        case '.':
+            return token_type::PERIOD;
+        case '[':
+            return token_type::LBRAC;
+        case ']':
+            return token_type::RBRAC;
+        case '#':
+            return token_type::HASH;
+        case '*':
+            return token_type::STAR;
+        case '-':
+            return token_type::DASH;
+        case '=':
+            return token_type::EQUALS;
+        case '<':
+            return token_type::LT;
+        case '>':
+            return token_type::GT;
+        case '|':
+            return token_type::PIPE;
+        case '`':
+            return token_type::TICK;
+        case '\\':
+            return token_type::ESCAPE;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            return token_type::NUMBER;
         default:
-            return "Unknown";
-        }
-    }
-
-    // tokens_equal
-    //    Returns true when two vectors of tokens are equal.
-    //    Used heavily in unit tests.
-    //
-    // Parameters:
-    //    const std::vector<Token> &tokens1 - The first vector of tokens.
-    //    const std::vector<Token> &tokens2 - The second vector of tokens.
-    //
-    // Returns:
-    //    bool - True when the two vectors of tokens are equal.
-    bool Token::tokens_equal(const std::vector<Token> &tokens1, const std::vector<Token> &tokens2)
-    {
-        if (tokens1.size() != tokens2.size())
-        {
-            return false;
-        }
-
-        const unsigned int size = tokens1.size();
-
-        for (unsigned int i = 0; i < size; i++)
-        {
-            if (tokens1[i] != tokens2[i])
-                return false;
-        }
-
-        return true;
-    }
-
-    // print_tokens
-    //    Returns a string representation of a vector of tokens.
-    //
-    // Parameters:
-    //    const std::vector<Token> &tokens - The vector of tokens.
-    //
-    // Returns:
-    //    std::string - The string representation of the vector of tokens.
-    std::string Token::print_tokens(const std::vector<Token> &tokens)
-    {
-        std::stringstream ss;
-        ss << "Tokens (" << tokens.size() << "):" << std::endl;
-        for (const Token token : tokens)
-        {
-            ss << Token::get_token_type(token.get_type()) << ": \"" << token.get_value() << "\"" << std::endl;
-        }
-        return ss.str();
+            return token_type::OTHER;
     }
 }
+
+}  // namespace sparkdown
+
+#pragma clang diagnostic pop
--- src/token/token.hpp
+++ src/token/token.hpp
@@ -1,181 +1,114 @@
-// //token
-//
-// Author: Cayden Lund
-//
-// This file is part of sparkdown, a new markup/markdown language
-// for quickly writing and formatting notes.
-//
-// This file contains the headers for the Token class.
-//
-// Copyright (C) 2021 Cayden Lund <https://github.com/shrimpster00>
-// License: MIT <https://opensource.org/licenses/MIT>
+/**
+ * @file token/token.hpp
+ * @package //token:token
+ * @author Cayden Lund <cayden.lund@utah.edu>
+ * @brief `token` class definition.
+ * @details This project is part of Sparkdown, a new markup language
+ *     for quickly writing and formatting notes.
+ *
+ *     This file defines the `token` class, which represents a single token
+ *     in the lexing/parsing process.
+ *
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @copyright 2021-2022 by Cayden Lund <https://github.com/caydenlund>
+ */
+
+// TODO: Remove temporary pragmas once we no longer need them.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma ide diagnostic ignored "OCUnusedStructInspection"
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "google-explicit-constructor"
 
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
-// System imports.
-#include <string>
-#include <sstream>
+namespace sparkdown {
 
-namespace sparkdown
-{
-    // An enumeration of the possible token types.
-    enum token_type
-    {
-        // Tokens that are used in the lexer and the parser.
-        SPACE,    // A space.             [ \t]+
-        DOLLAR,   // A dollar sign.       $
-        COLON,    // A colon.             :
-        LBRAC,    // A left bracket.      [
-        RBRAC,    // A right bracket.     ]
-        HASH,     // A hash.              #
-        STAR,     // An asterisk.         *
-        DASH,     // A dash.              [-]+
-        EQUALS,   // An equals sign.      [=]+
-        LT,       // A less-than sign.    <
-        GT,       // A greater-than sign. >
-        PIPE,     // A vertical bar.      |
-        TICK,     // A backtick.          [`]+
-        ESCAPE,   // A backslash.
-        NUMBER,   // A numeral.           [0-9]+
-        TERMINAL, // A string that should be passed through as-is.
+/**
+ * @brief An enumeration of the possible token types.
+ *
+ */
+enum token_type {
+    SPACE,   // A space.              [ \t]
+    DOLLAR,  // A dollar sign.        '$'
+    COLON,   // A colon.              ':'
+    PERIOD,  // A period.             '.'
+    LBRAC,   // A left bracket.       '['
+    RBRAC,   // A right bracket.      ']'
+    HASH,    // A hash.               '#'
+    STAR,    // An asterisk.          '*'
+    DASH,    // A hyphen.             '-'
+    EQUALS,  // An equals sign.       '='
+    LT,      // A less-than sign.     '<'
+    GT,      // A greater-than sign.  '>'
+    PIPE,    // A vertical bar.       '|'
+    TICK,    // A backtick.           '`'
+    ESCAPE,  // A backslash.          '\'
+    NUMBER,  // A numeral.            [0-9]+
+    OTHER    // Anything else.
+};
 
-        // The following tokens are not used by the lexer;
-        // rather, they have special syntactic meaning and are created by the parser.
-        HEADER,      // A header command.
-        SECTION,     // A section headline.
-        ARROW,       // An arrow.
-        BULLET,      // A bullet point.
-        NUMBERED,    // A numbered list item.
-        BOLD,        // Bold text.
-        ITALIC,      // Italic text.
-        INLINE_MATH, // An inline math block.
-        INLINE_VERB  // An inline verbatim block.
-    };
+/**
+ * @brief Represents a single character in a string of Sparkdown text.
+ * @details The lexer will create a list of tokens from a string.
+ *     These tokens are then used by the parser to parse the sequence of tokens
+ *     into LaTeX code.
+ *
+ */
+struct token {
+    /**
+     * @brief Constructs a new token from a single character.
+     * @details The implicit construction is intentional.
+     *
+     * @param character The character used to construct the token.
+     */
+    token(char character);
 
-    // Token
-    //    This class is the token class, used to store the type and value of a token.
-    //    The Lexer class will create a list of tokens from a string.
-    //    These tokens are then used by the Parser class to parse the input line into LaTeX code.
-    class Token
-    {
-    public:
-        // constructor
-        //    The one-argument constructor saves the type of the token.
-        //
-        // Parameters:
-        //    const token_type &type - The type of the token.
-        Token(const token_type &type);
+    /**
+     * @brief Copy constructor.
+     *
+     * @param other The other token to copy.
+     */
+    token(const token &other);
 
-        // constructor
-        //    The two-argument constructor saves the type and value of the token.
-        //
-        // Parameters:
-        //    const token_type &type        - The type of the token.
-        //    const std::string_view &value - The value of the token.
-        //       OR
-        //    const char &value             - The value of the token.
-        Token(const token_type &type, const std::string &value);
-        Token(const token_type &type, const char &value);
+    /**
+     * @brief Equality comparison operator.
+     *
+     * @param other The other token to compare with this one.
+     * @return Whether the tokens are equal.
+     */
+    bool operator==(const token &other) const;
 
-        // constructor
-        //    The copy constructor saves the type and value of the token.
-        //
-        // Parameters:
-        //    const Token &other - The token to copy.
-        Token(const Token &other);
+    /**
+     * @brief Inequality comparison operator.
+     *
+     * @param other The other token to compare with this one.
+     * @return Whether the tokens are unequal.
+     */
+    bool operator!=(const token &other) const;
 
-        // merge
-        //    Merges two tokens together.
-        //
-        // Parameters:
-        //    const Token &other - The token to merge with.
-        void merge(const Token &other);
+    /**
+     * @brief Given a character, returns the corresponding token type.
+     *
+     * @param character The character to test.
+     * @return The corresponding token type.
+     */
+    static token_type get_type(char character);
 
-        // get_type
-        //    Returns the type of the token.
-        //
-        // Returns:
-        //    token_type - The type of the token.
-        token_type get_type() const;
+    /**
+     * @brief The type of the token.
+     *
+     */
+    const token_type type;
 
-        // get_value
-        //    Returns the string value of the token.
-        //
-        // Returns:
-        //    std::string - The value of the token.
-        std::string get_value() const;
-
-        // = operator
-        //    Allow for assignment of tokens.
-        //
-        // Parameters:
-        //    const Token &other - The token to assign.
-        void operator=(const Token &other);
-
-        // == operator
-        //    Compare two tokens for equality.
-        //
-        // Parameters:
-        //    const Token &other - The token with which to compare.
-        //
-        // Returns:
-        //    bool - True when the tokens are equal.
-        bool operator==(const Token &other) const;
-
-        // != operator
-        //    Compare two tokens for inequality.
-        //
-        // Parameters:
-        //    const Token &other - The token with which to compare.
-        //
-        // Returns:
-        //    bool - True when the tokens are not equal.
-        bool operator!=(const Token &other) const;
-
-        // ===========================
-        // || Public static methods ||
-        // ===========================
-
-        // get_token_type
-        //    Returns the string representation of the token type.
-        //
-        // Parameters:
-        //    const token_type &type - The type of the token.
-        //
-        // Returns:
-        //    std::string - The string representation of the token type.
-        static std::string get_token_type(const token_type &type);
-
-        // tokens_equal
-        //    Returns true when two vectors of tokens are equal.
-        //    Used heavily in unit tests.
-        //
-        // Parameters:
-        //    const std::vector<Token> &tokens1 - The first vector of tokens.
-        //    const std::vector<Token> &tokens2 - The second vector of tokens.
-        //
-        // Returns:
-        //    bool - True when the two vectors of tokens are equal.
-        static bool tokens_equal(const std::vector<Token> &tokens1, const std::vector<Token> &tokens2);
-
-        // print_tokens
-        //    Returns a string representation of a vector of tokens.
-        //
-        // Parameters:
-        //    const std::vector<Token> &tokens - The vector of tokens.
-        //
-        // Returns:
-        //    std::string - The string representation of the vector of tokens.
-        static std::string print_tokens(const std::vector<Token> &tokens);
-
-    protected:
-        // The type of the token.
-        token_type type;
-
-        // The value of the token.
-        std::stringstream value;
-    };
-}
+    /**
+     * @brief The value of the token.
+     *
+     */
+    const char value;
+};
+}  // namespace sparkdown
 
 #endif
+#pragma clang diagnostic pop
